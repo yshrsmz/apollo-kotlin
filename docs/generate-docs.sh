b2a73c02b2fdd3933d1489f9aacc1ebae5f94213
Trevor Blades <tdblades@gmail.com>
2022-03-17 13:13:03 -0700

    Convert docs config to yaml

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -4,5 +4,3 @@
 set -e
 
 echo -e "---\ntitle: Introduction\ndescription: A strongly-typed, caching GraphQL client for the JVM, Android and Kotlin multiplatform\n---\n\n$(cat ../README.md | grep -v 'the official docs')" > source/index.md
-
-gatsby build
